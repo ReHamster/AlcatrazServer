ffa7b012c9db7b5f74afb64ec12eaddaed4d0baa
Ilya Shurumov <ishurumov@gmail.com>
2021-08-03 16:10:06 +0600

    - new RMC factory for easier coding - rename protocols

---
+++ QuazalWV/Interfaces/RMCContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuazalWV.Interfaces
+{
+	public class RMCContext
+	{
+		public RMCContext(RMCP rmc, ClientInfo clientInfo, QPacket packet)
+		{
+			RMC = rmc;
+			Client = clientInfo;
+			Packet = packet;
+		}
+
+		public readonly RMCP RMC;
+		public readonly ClientInfo Client;
+		public readonly QPacket Packet;
+	}
+}
---
+++ QuazalWV/Interfaces/RMCServiceBase.cs
@@ -0,0 +1,45 @@
+﻿using QuazalWV.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuazalWV.Interfaces
+{
+	public class RMCServiceBase
+	{
+		RMCContext _context;
+
+		public RMCContext Context {
+			get { return _context; }
+			set {
+				// make it so user won't override it
+				if(_context == null)
+					_context = value;
+			} 
+		}
+
+		protected void SendResponseWithACK(RMCPResponse reply, bool useCompression = true, uint error = 0)
+		{
+			RMC.SendResponseWithACK(_context.Client.udp, _context.Packet, _context.RMC, _context.Client, reply, useCompression, error);
+		}
+
+		protected void UNIMPLEMENTED()
+		{
+			var stackTrace = new StackTrace();
+			var method = stackTrace.GetFrame(1).GetMethod();
+
+			var methodName = method.Name;
+
+			var rmcMethodAttr = (RMCMethodAttribute)method.GetCustomAttributes(typeof(RMCMethodAttribute), true).SingleOrDefault();
+			if(rmcMethodAttr != null && !string.IsNullOrWhiteSpace(rmcMethodAttr.Name))
+				methodName = rmcMethodAttr.Name;
+
+			Log.WriteLine(1, $"Error: Method '{ methodName }' is unimplemented in '{ _context.RMC.proto }'");
+		}
+	}
+}
