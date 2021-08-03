6f95ea690fc55dfd29fb546b518bb1efa00b8fde
Ilya Shurumov <ishurumov@gmail.com>
2021-08-03 23:56:34 +0600

    - implemented writing DDL objects - added RMCPResponseDDL generic encapsulation

---
+++ QuazalWV/RMC/RMCPResponseDDL.cs
@@ -0,0 +1,37 @@
+﻿using QuazalWV.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuazalWV
+{
+	class RMCPResponseDDL<T> : RMCPResponse where T: class
+	{
+		public RMCPResponseDDL(T data)
+		{
+			objectData = data;
+		}
+		T objectData;
+
+		public override string PayloadToString()
+		{
+			return "";
+		}
+
+		public override byte[] ToBuffer()
+		{
+			var m = new MemoryStream();
+			DDLHelper.WriteObject(objectData, m);
+
+			return m.ToArray();
+		}
+
+		public override string ToString()
+		{
+			return $"[RMCPResponseDDL<{typeof(T).Name}>";
+		}
+	}
+}
--- QuazalWV/RMCServices/UbiWinService.cs
+++ QuazalWV/RMCServices/UbiWinService.cs
@@ -1,8 +1,33 @@
 ﻿using QuazalWV.Attributes;
 using QuazalWV.Interfaces;
+using System.Collections.Generic;
+using System.IO;
 
 namespace QuazalWV.RMCServices
 {
+	public class UplayActionPlatform
+	{
+		public string m_platformCode { get; set; }
+		public bool m_completed { get; set; }
+		public string m_specificKey { get; set; }
+	}
+
+	public class UplayAction
+	{
+		public UplayAction()
+		{
+			m_platforms = new List<UplayActionPlatform>();
+		}
+
+		public string m_code { get; set; }
+		public string m_name { get; set; }
+		public string m_description { get; set; }
+		public ushort m_value { get; set; }
+		public string m_gameCode { get; set; }
+
+		public IEnumerable<UplayActionPlatform> m_platforms { get; set; }
+	}
+
 	/// <summary>
 	/// User friends service
 	/// </summary>
@@ -48,7 +73,10 @@ namespace QuazalWV.RMCServices
 		[RMCMethod(7)]
         public void UplayWelcome(string culture, string platformCode)
 		{
-			UNIMPLEMENTED();
+			var result = new List<UplayAction>();
+
+			var reply = new RMCPResponseDDL<List<UplayAction>>(result);
+			SendResponseWithACK(reply);
 		}
 
 		[RMCMethod(8 )]
