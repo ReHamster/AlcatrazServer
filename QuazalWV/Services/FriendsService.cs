e5b9bd9eb6e487dd0f5a85c3717eaaca449ffb8e
Ilya Shurumov <ishurumov@gmail.com>
2021-08-09 01:33:29 +0600

    - implemented ReadPlayerStats, partially WritePlayerStats

--- QuazalWV/Services/FriendsService.cs
+++ QuazalWV/Services/FriendsService.cs
@@ -1,6 +1,8 @@
 ﻿using QuazalWV.Attributes;
+using QuazalWV.DDL;
 using QuazalWV.Interfaces;
 using System.Collections.Generic;
+using System.IO;
 
 namespace QuazalWV.Services
 {
@@ -80,16 +82,17 @@ namespace QuazalWV.Services
 		public RMCResult GetDetailedList(byte byRelationship, bool bReversed)
 		{
 			var result = new List<FriendData>();
-
+			UNIMPLEMENTED();
 			return Result(result);
 		}
 
 		[RMCMethod(13)]
 		public RMCResult GetRelationships(int offset, int size)
 		{
-			// 20 01 00 00 14 01 17 00 00 00 0D 80 00 00 0B 00 00 00 0B 00 00 00 47 BD 05 00 0E 00 56 6F 72 74 65 78 4C 65 42 65 6C 67 65 00 01 00 00 00 00 00 AB D6 05 00 09 00 6D 63 6E 61 6C 6C 79 6F 00 01 00 00 00 00 00 E9 3B 08 00 0C 00 53 6E 6F 6F 70 79 42 6C 61 6E 6B 00 01 00 00 00 00 00 22 3D 08 00 0B 00 7A 75 63 6B 69 6C 6F 61 6B 73 00 01 00 00 00 00 00 B5 43 08 00 0F 00 67 6F 6C 64 65 6E 5F 73 6C 65 6E 64 65 72 00 01 00 00 00 00 00 23 44 08 00 0C 00 4E 69 6B 6B 69 43 68 61 6E 39 32 00 01 00 00 00 00 00 D0 45 08 00 0C 00 61 64 72 69 61 61 6E 39 31 30 30 00 01 00 00 00 00 00 2B 47 08 00 0E 00 52 61 63 69 6E 67 46 72 65 61 6B 39 35 00 01 00 00 00 00 00 8C 4D 08 00 0E 00 56 65 72 79 48 6F 74 50 65 72 73 6F 6E 00 01 00 00 00 00 00 F3 4D 08 00 0C 00 56 6F 72 74 65 78 53 74 6F 72 65 00 01 00 00 00 00 00 97 4F 08 00 0D 00 53 49 44 45 53 57 49 50 45 31 32 37 00 01 00 00 00 00 00 
+			// 
 			var result = new RelationshipsResult();
 
+			UNIMPLEMENTED();
 			return Result(result);
 		}
 	}
--- QuazalWV/Services/RichPresenceService.cs
+++ QuazalWV/Services/RichPresenceService.cs
@@ -2,21 +2,31 @@
 using QuazalWV.Interfaces;
 using QuazalWV.DDL.Models;
 using System.Collections.Generic;
+using QuazalWV.DDL;
 
 namespace QuazalWV.Services
 {
 	[RMCService(RMCProtocolId.RichPresenceService)]
 	public class RichPresenceService : RMCServiceBase
 	{
+		private static List<PresenceElement> presences;
+
 		[RMCMethod(1)]
-		public RMCResult SetPresence(uint type)
+		public RMCResult SetPresence(int phraseId, qBuffer argument)
 		{
-			UNIMPLEMENTED();
+			/*
+			presences.Add(new PresenceElement()
+			{
+				isConnected = true,
+				phraseId = phraseId,
+				principalId = Context.Client.info.PID,
+				argument = argument.data,
+			});*/
 			return Error(0);
 		}
 
 		[RMCMethod(2)]
-		public RMCResult GetPresence(uint type)
+		public RMCResult GetPresence(IEnumerable<uint> pids)
 		{
 			// ret: 0A 00 00 00 6D 01 1A 00 00 00 01 80 00 00 
 			UNIMPLEMENTED();
