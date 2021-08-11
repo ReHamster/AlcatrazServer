e2d295cf5d25a619dd5fa2e2fc5586434bcff8b4
Ilya Shurumov <ishurumov@gmail.com>
2021-08-11 22:00:54 +0600

    - NATTraversalService stub

---
+++ RDVServices/DDL/Models/NATTraversalService/RelaySignatureKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDVServices.DDL.Models
+{
+	public class RelaySignatureKey
+	{
+		public int relayMode { get; set; }
+		public DateTime currentUTCTime { get; set; }
+		public string address { get; set; }
+		public ushort port { get; set; }
+		public int relayAddressType { get; set; }
+		public uint gameServerID { get; set; }
+	}
+}
