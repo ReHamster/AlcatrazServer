38facf91da7f9c5d10f816193f4d6525c86401f5
Ilya Shurumov <ishurumov@gmail.com>
2021-08-11 21:49:04 +0600

    - implement CreateUniqueID

--- DSFServices/Services/DriverUniqueIDService.cs
+++ DSFServices/Services/DriverUniqueIDService.cs
@@ -7,16 +7,12 @@ namespace DSFServices.Services
 	[RMCService(RMCProtocolId.DriverUniqueIDService)]
 	public class DriverUniqueIDService : RMCServiceBase
 	{
+		static uint UniqueIDCounter = 26434; // FIXME: is it too simplistic?
+
 		[RMCMethod(2)]
 		public RMCResult CreateUniqueID()
 		{
-			uint uniqueID = 26434;
-
-			UNIMPLEMENTED();
-
-			// return 0E 00 00 00 76 01 5A 00 00 00 02 80 00 00 42 67 00 00 
-
-			return Result(new { value = uniqueID });
+			return Result(new { value = ++UniqueIDCounter });
 		}
 	}
 }
