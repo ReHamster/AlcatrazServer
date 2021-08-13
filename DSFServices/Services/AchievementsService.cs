6a2299ab0067cdb77547e26ae32fae836349450c
Ilya Shurumov <ishurumov@gmail.com>
2021-08-14 00:09:05 +0600

    - added AchievementsService & UnlockAchievements stub

---
+++ DSFServices/Services/AchievementsService.cs
@@ -0,0 +1,21 @@
+﻿using DSFServices.DDL.Models;
+using QNetZ;
+using QNetZ.Attributes;
+using QNetZ.DDL;
+using QNetZ.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DSFServices.Services
+{
+	[RMCService(RMCProtocolId.AchievementsService)]
+	public class AchievementsService : RMCServiceBase
+	{
+		[RMCMethod(3)]
+		public RMCResult UnlockAchievements(IEnumerable<int> achievementIds)
+		{
+			UNIMPLEMENTED();
+			return Error(0);
+		}
+	}
+}
