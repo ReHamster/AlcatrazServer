17c44ee409777f8d0ba523ab15e16600fd6d3f29
Ilya Shurumov <ishurumov@gmail.com>
2021-08-14 20:52:04 +0600

    - save relationship details, not status

--- AlcatrazDbContext/Migrations/20210814143040_FriendsUpdate.Designer.cs
+++ AlcatrazDbContext/Migrations/20210814143040_FriendsUpdate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Alcatraz.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace AlcatrazDbContext.Migrations
+namespace Alcatraz.Context.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210814143040_FriendsUpdate")]
+    partial class FriendsUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
