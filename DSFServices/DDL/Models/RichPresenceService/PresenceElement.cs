8231bb10bc763802735e87029d43ce7e95854acd
Ilya Shurumov <ishurumov@gmail.com>
2021-08-18 18:37:47 +0600

    - PresenceElement: byte[] to qBuffer

--- DSFServices/DDL/Models/RichPresenceService/PresenceElement.cs
+++ DSFServices/DDL/Models/RichPresenceService/PresenceElement.cs
@@ -1,10 +1,12 @@
-﻿namespace DSFServices.DDL.Models
+﻿using QNetZ.DDL;
+
+namespace DSFServices.DDL.Models
 {
 	public class PresenceElement
 	{
 		public uint principalId { get; set; }
 		public bool isConnected { get; set; }
 		public int phraseId { get; set; }
-		public byte[] argument { get; set; }
+		public qBuffer argument { get; set; }
 	}
 }
