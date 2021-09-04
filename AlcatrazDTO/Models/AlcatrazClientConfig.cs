d468e8ac9a92437e5bc54c68e17c59d146a3abd5
Ilya Shurumov <ishurumov@gmail.com>
2021-09-05 01:04:40 +0600

    - added DTO shared project

---
+++ AlcatrazDTO/Models/AlcatrazClientConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alcatraz.DTO.Models
+{
+	public class OnlineConfig
+	{
+		public bool Use { get; set; }
+		public string ServiceUrl { get; set; }
+		public string ConfigKey { get; set; }
+		public string AccessKey { get; set; }
+	}
+
+	public class ProfileConfig
+	{
+		public string AccountId { get; set; }
+		public string Password { get; set; }
+		public string GameKey { get; set; }
+	}
+
+	public class AlcatrazClientConfig
+	{
+		public OnlineConfig OnlineConfig { get; set; }
+		public ProfileConfig Profile { get; set; }
+	}
+}
---
+++ AlcatrazDTO/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alcatraz.DTO.Models
+{
+	public class UserModel
+	{
+		public uint Id { get; set; }
+		public string Username { get; set; }
+		public string PlayerNickName { get; set; }
+	}
+}
