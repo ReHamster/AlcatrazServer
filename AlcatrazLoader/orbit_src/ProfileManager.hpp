32452af13fa1ef010793447372d8fdcd744a8bd7
Ilya Shurumov <ishurumov@gmail.com>
2021-09-05 01:04:12 +0600

    - loader - redone profile management

---
+++ AlcatrazLoader/orbit_src/ProfileManager.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Objects/OrbitConfig.hpp"
+#include "Helpers/Failure.hpp"
+#include "Utils/Singleton.hpp"
+
+#include "HF/HackingFrameworkFWD.h"
+
+#include "HF/HackingFramework.hpp"
+
+namespace AlcatrazUplayR2
+{
+	struct ProfileData
+	{
+		String Name;
+
+		String AccountId;
+		String Password;
+		String GameKey;
+
+		String ServiceUrl;
+		String ConfigKey;
+		String SandboxAccessKey;
+	};
+
+	inline void InitProfile()
+	{
+		ProfileData profile;
+
+		auto& config = Singleton<OrbitConfig>::Instance().Get();
+		auto& AllProfiles = config.dataVariant.toMap().find("Profiles")->toMap();
+
+		profile.Name = config.dataVariant.toMap().find("UseProfile")->toString();
+		auto& currentProfile = AllProfiles.find(profile.Name)->toMap();
+
+		profile.AccountId = currentProfile.find("AccountId")->toString();
+		profile.Password = currentProfile.find("Password")->toString();
+		profile.GameKey = currentProfile.find("GameKey")->toString();
+		profile.ServiceUrl = currentProfile.find("ServiceUrl")->toString();
+		profile.ConfigKey = currentProfile.find("ConfigKey")->toString();
+		profile.SandboxAccessKey = currentProfile.find("AccessKey")->toString();
+
+		Singleton<ProfileData>::Instance().Set(profile);
+	}
+}
