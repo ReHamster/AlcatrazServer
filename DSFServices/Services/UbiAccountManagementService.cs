97b8292aa6b91906f7c746cacbabc1720ab71a8d
Ilya Shurumov <ishurumov@gmail.com>
2022-07-16 16:31:29 +0600

    - correct data for UbiAccount.GetAccount()

--- DSFServices/Services/UbiAccountManagementService.cs
+++ DSFServices/Services/UbiAccountManagementService.cs
@@ -1,9 +1,11 @@
 ﻿using DSFServices.DDL.Models;
 using QNetZ;
 using QNetZ.Attributes;
+using QNetZ.DDL;
 using QNetZ.Interfaces;
 using RDVServices;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace DSFServices.Services
@@ -34,13 +36,45 @@ namespace DSFServices.Services
 			{
 				m_ubiAccountId = playerInfo.AccountId,
 				m_username = playerInfo.Name,
-				m_password = "[REDACTED]",
-				m_firstName = "Soapy",
-				m_lastName = "Man",
-				m_countryCode = "kz",
-				m_email = "dumb@ass.com",
+				m_password = "",
+				m_firstName = "",
+				m_lastName = "",
+				m_countryCode = "KZ",
+				m_email = "whatever@dontcare.com",
 				m_preferredLanguage = "en",
 				m_gender = 0,
+				m_optIn = true,
+				m_thirdPartyOptIn = true,
+				m_status = new UbiAccountStatus()
+                {
+					m_basicStatus = 2,
+					m_missingRequiredInformations = false,
+					m_pendingDeactivation = false,
+					m_recoveringPassword = true
+                },
+				m_externalAccounts = new List<ExternalAccount>()
+                {
+					new ExternalAccount()
+                    {
+						m_accountType = 11,
+						m_id = "loh",
+						m_username = "aabb0"
+                    },
+					new ExternalAccount()
+					{
+						m_accountType = 25,
+						m_id = "pidr",
+						m_username = "aabb1"
+					},
+					new ExternalAccount()
+					{
+						m_accountType = 31,
+						m_id = "whatev",
+						m_username = "aabb2"
+					}
+
+				},
+				m_dateOfBirth = new System.DateTime(1990, 11, 1)
 			};
 			
 			return Result(reply);
