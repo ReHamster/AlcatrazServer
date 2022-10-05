430d4d032afafc35838d4b29b685703dc6ccd3d9
Ilya Shurumov <ishurumov@gmail.com>
2022-10-05 14:09:51 +0600

    - add RemoteLogDevice service

---
+++ RDVServices/Services/RemoteLogDeviceService.cs
@@ -0,0 +1,17 @@
+﻿using QNetZ;
+using QNetZ.Attributes;
+using QNetZ.Interfaces;
+
+namespace RDVServices.Services
+{
+	[RMCService(RMCProtocolId.RemoteLogDeviceService)]
+	public class RemoteLogDeviceService : RMCServiceBase
+	{
+		[RMCMethod(1)]
+		public RMCResult Log(string strLine)
+		{
+			QLog.WriteLine(1, $"Recieved from PID={Context.Client.Info.PID}: {strLine}");
+			return Error(0);
+		}
+	}
+}
