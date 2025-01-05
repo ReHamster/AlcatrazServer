d6f41b4b0832f6e7b83e9d3e26a6d15b7c468616
SoapyMan <ishurumov@gmail.com>
2025-01-06 01:52:24 +0500

    - silence DFEngine

--- AlcatrazLoader/noads_src/dfengine.cpp
+++ AlcatrazLoader/noads_src/dfengine.cpp
@@ -10,25 +10,25 @@ public:
 	virtual int __stdcall Initialize(int version, int* arguments) = 0;	// 4
 	virtual void __stdcall Shutdown() = 0;	// 8
 
-	virtual void __stdcall _padFunc1() { printf("%s\n", __FUNCTION__); }// 12
-	virtual int __stdcall _padFunc2(int a0, int a1, int* ptr1, int* ptr2) { printf("%s\n", __FUNCTION__); return -1; }// 16
-	virtual void __stdcall _padFunc3() { printf("%s\n", __FUNCTION__); }// 20
-	virtual void __stdcall _padFunc4() { printf("%s\n", __FUNCTION__); }// 24
-	virtual void __stdcall _padFunc5() { printf("%s\n", __FUNCTION__); }// 28
-	virtual void __stdcall _padFunc6() { printf("%s\n", __FUNCTION__); }// 32
-	virtual void __stdcall _padFunc7(const char* arg) { printf("%s\n", __FUNCTION__); }// 36
-	virtual void __stdcall _padFunc8() { printf("%s\n", __FUNCTION__); }// 40
-	virtual void __stdcall _padFunc9() { printf("%s\n", __FUNCTION__); }// 44
-	virtual void __stdcall _padFunc10() { printf("%s\n",__FUNCTION__); }// 48
-	virtual void __stdcall _padFunc11() { printf("%s\n",__FUNCTION__); }// 52
-	virtual void __stdcall _padFunc12(int arg) { printf("%s\n",__FUNCTION__); }// 56
-	virtual void __stdcall _padFunc13(int arg) { printf("%s\n",__FUNCTION__); }// 60
-	virtual void __stdcall _padFunc14() { printf("%s\n",__FUNCTION__); }// 64
-	virtual void __stdcall _padFunc15() { printf("%s\n",__FUNCTION__); }// 68
-	virtual int __stdcall _padFunc16(int arg) { printf("%s\n", __FUNCTION__); return 0; }// 72
-	virtual void __stdcall _padFunc17() { printf("%s\n",__FUNCTION__); }// 76
-	virtual void __stdcall _padFunc18() { printf("%s\n",__FUNCTION__); }// 80
-	virtual void __stdcall _padFunc19(int arg) { printf("%s\n",__FUNCTION__); }// 84
+	virtual void __stdcall _padFunc1() { }// 12
+	virtual int __stdcall _padFunc2(int a0, int a1, int* ptr1, int* ptr2) {  return -1; }// 16
+	virtual void __stdcall _padFunc3() { }// 20
+	virtual void __stdcall _padFunc4() { }// 24
+	virtual void __stdcall _padFunc5() { }// 28
+	virtual void __stdcall _padFunc6() { }// 32
+	virtual void __stdcall _padFunc7(const char* arg) {  }// 36
+	virtual void __stdcall _padFunc8() { }// 40
+	virtual void __stdcall _padFunc9() { }// 44
+	virtual void __stdcall _padFunc10() { }// 48
+	virtual void __stdcall _padFunc11() { }// 52
+	virtual void __stdcall _padFunc12(int arg) {  }// 56
+	virtual void __stdcall _padFunc13(int arg) {  }// 60
+	virtual void __stdcall _padFunc14() { }// 64
+	virtual void __stdcall _padFunc15() { }// 68
+	virtual int __stdcall _padFunc16(int arg) { return 0; }// 72
+	virtual void __stdcall _padFunc17() { }// 76
+	virtual void __stdcall _padFunc18() { }// 80
+	virtual void __stdcall _padFunc19(int arg) {  }// 84
 };
 
 class CDummy : public IDummyInterface
